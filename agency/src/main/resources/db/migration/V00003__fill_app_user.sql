63dbe87a5bc9611c76f1fad6c0a9dc18c439e9fd
alexyekymov <alexyekymov@gmail.com>
2026-06-23 14:35:07 +0300

    chore(migration): fix password hashes

--- agency/src/main/resources/db/migration/V00003__fill_app_user.sql
+++ agency/src/main/resources/db/migration/V00003__fill_app_user.sql
@@ -1,7 +1,7 @@
 INSERT INTO app_user
 (id, first_name, last_name, email, password, phone_number, account_status)
-VALUES ('019ef133-47c6-78fb-b57a-474fc31850d6', 'Olha', 'Kovalenko', 'olha.kovalenko@example.com','$2a$10$4flWgjeRgVvFFSmnsF1EjuaJIwPNvjtVyUeLR09o0.iN9wGn6yMQC', '+380501112201', true),
-       ('019ef133-8ad7-7de9-8953-ff95a41361c1','Andriy', 'Shevchenko', 'andriy.shevchenko@example.com','$2a$10$4flWgjeRgVvFFSmnsF1EjuaJIwPNvjtVyUeLR09o0.iN9wGn6yMQC', '+380501112202', true),
-       ('019ef133-be7d-7d1d-ba1a-4618d3170b42', 'Anna', 'Petrenko', 'anna.petrenko@example.com', '$2a$10$4flWgjeRgVvFFSmnsF1EjuaJIwPNvjtVyUeLR09o0.iN9wGn6yMQC', '+380501112217', true),
-       ('019ef133-cf4e-7f79-a6a4-e2ed0dc488bf', 'Ihor', 'Moroz', 'ihor.moroz@example.com', '$2a$10$4flWgjeRgVvFFSmnsF1EjuaJIwPNvjtVyUeLR09o0.iN9wGn6yMQC', '+380501112218', true),
-       ('019ef134-0c2f-7c88-9fba-4baaf6c0a271', 'Viktoriia', 'Symonenko', 'viktoriia.symonenko@example.com', '$2a$10$4flWgjeRgVvFFSmnsF1EjuaJIwPNvjtVyUeLR09o0.iN9wGn6yMQC', '+380501112219', true);
+VALUES ('019ef133-47c6-78fb-b57a-474fc31850d6', 'Olha', 'Kovalenko', 'olha.kovalenko@example.com','{bcrypt}$2a$10$4flWgjeRgVvFFSmnsF1EjuaJIwPNvjtVyUeLR09o0.iN9wGn6yMQC', '+380501112201', true),
+       ('019ef133-8ad7-7de9-8953-ff95a41361c1','Andriy', 'Shevchenko', 'andriy.shevchenko@example.com','{bcrypt}$2a$10$4flWgjeRgVvFFSmnsF1EjuaJIwPNvjtVyUeLR09o0.iN9wGn6yMQC', '+380501112202', true),
+       ('019ef133-be7d-7d1d-ba1a-4618d3170b42', 'Anna', 'Petrenko', 'anna.petrenko@example.com', '{bcrypt}$2a$10$4flWgjeRgVvFFSmnsF1EjuaJIwPNvjtVyUeLR09o0.iN9wGn6yMQC', '+380501112217', true),
+       ('019ef133-cf4e-7f79-a6a4-e2ed0dc488bf', 'Ihor', 'Moroz', 'ihor.moroz@example.com', '{bcrypt}$2a$10$4flWgjeRgVvFFSmnsF1EjuaJIwPNvjtVyUeLR09o0.iN9wGn6yMQC', '+380501112218', true),
+       ('019ef134-0c2f-7c88-9fba-4baaf6c0a271', 'Viktoriia', 'Symonenko', 'viktoriia.symonenko@example.com', '{bcrypt}$2a$10$4flWgjeRgVvFFSmnsF1EjuaJIwPNvjtVyUeLR09o0.iN9wGn6yMQC', '+380501112219', true);
